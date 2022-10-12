622505059bc25cd63821ddff3c3f4a5e3338e22c
zewa1999 <costacheandrei43@gmail.com>
2022-10-12 13:32:58 +0300

    Added base project

---
+++ MonkeyFinder/GlobalUsings.cs
@@ -0,0 +1,7 @@
+﻿global using MonkeyFinder.Model;
+global using MonkeyFinder.ViewModel;
+global using System.Collections.ObjectModel;
+global using System.ComponentModel;
+global using System.Diagnostics;
+global using System.Runtime.CompilerServices;
+global using System.Text.Json;
